d00cb8876caac1edf6f60b617f0d851f8e34823c
Uederson Ferreira <uedersonferreira@gmail.com>
2020-10-31 23:22:27 -0300

    Comidas da Cobrinha

--- Jogo da Cobrinha/script.js
+++ Jogo da Cobrinha/script.js
@@ -9,6 +9,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -17,23 +21,44 @@ function criarBG(){
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "red";
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function iniciarJogo (){
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 /*&& direction != "right"*/) direction = "left";
+    if(event.keyCode == 38 /*&& direction != "down"*/) direction = "up";
+    if(event.keyCode == 39 /*&& direction != "left"*/) direction = "right";
+    if(event.keyCode == 40 /*&& direction != "up"*/) direction = "down";
+    console.log(event.keyCode);
+}
+
+function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-
-    if(direction == "up") snakeY += box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+    console.log(direction);
 
     snake.pop();
 
@@ -45,4 +70,4 @@ function iniciarJogo (){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100)
+let jogo = setInterval(iniciarJogo, 200)
